f44f86a9a6e054e8bb7c76ca4e9a8cdf13cba87f
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-20 09:19:48 -0700

    Create Strrev.java

---
+++ Strrev.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Reverse {
+ public static void main(String[] args) {
+      
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+StringBuffer ss=new StringBuffer(str);
+ss.reverse();
+System.out.println(ss);
+ }}
+      
